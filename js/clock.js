cba0a3fa68ef6f54046510e6fc1bc2e3bc3e25e0
David Pryor <dapryor@ncsu.edu>
2015-11-04 23:36:10 -0500

    Fixed clock

--- js/clock.js
+++ js/clock.js
@@ -1,18 +1,18 @@
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('txt').innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
-}
+
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    "use strict";
+    if (i < 10) {i = "0" + i; }  // add zero in front of numbers < 10
     return i;
 }
+function startTime() {
+    "use strict";
+    var today = new Date(), h = today.getHours(), m = today.getMinutes(), s = today.getSeconds(), t = setTimeout(startTime, 500);
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
+
+}
+
 
 
 
